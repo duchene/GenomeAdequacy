7f291ab969ab414e533095302947d557b27ab42a
dvchene <duchene>
2016-01-04 11:51:06 +1100

    New genomes added. Old unnecessary genomes removed.

---
+++ clean.gene.R
@@ -0,0 +1,13 @@
+require(ape)
+
+clean.gene <- function(x){
+	   gene <- as.character(x)
+	   goodcols <- vector()
+	   for(i in 1:ncol(gene)){
+	   	 if(length(grep("a|c|g|t", gene[, i])) == nrow(gene)) goodcols <- c(goodcols, i)
+	   }
+	   if(length(goodcols) == 0 || length(goodcols) == 1) stop("Too many sites have missing data")
+	   gene <- gene[, goodcols]
+	   print(length(goodcols))
+	   return(as.DNAbin(gene))
+}
